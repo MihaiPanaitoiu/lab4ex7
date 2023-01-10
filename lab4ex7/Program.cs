03462f682a45b6080bed0976329804626a306343
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2023-01-10 23:15:14 +0200

    Add project files.

---
+++ lab4ex7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab4ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti n");
+            int n = int.Parse(Console.ReadLine());
+            WritePyramid(n);
+        }
+
+        static void WritePyramid(int n, int i = 1)
+        {
+
+            if (n == 0)
+            {
+                return;
+            }
+
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine();
+            WritePyramid(n-1, i+1);
+
+        }
+    }
+}
